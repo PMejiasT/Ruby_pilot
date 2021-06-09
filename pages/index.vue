c521bd91f98996eef4a0830e1a642f91e70ba120
Angela Blanes <angelablanes@goldcar.tech>
2021-06-09 14:01:47 +0200

    inicio

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>Inicio</h1>
-    <img src="~/assets/img/pokemon-ruby.png" />
-  </div>
+    <div class="imagenRuby">
+      <img src="~/assets/img/pokemon-ruby.png" />
+    </div>
 </template>
 
 <script>
@@ -12,3 +13,29 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.imagenRuby{
+  display:inline-block;
+  width:800px;
+  height:600px;
+  box-sizing:border-box;
+  margin-top:40px;
+  position:relative;
+}
+.column, .blind, .blindImg{
+  position:absolute;
+  top:0;
+  height:100%;
+  box-sizing:border-box;
+}
+
+.blind{
+  overflow:hidden;
+}
+
+.blindImg{
+  width:100%;
+  background-size:cover;
+}
+</style>
