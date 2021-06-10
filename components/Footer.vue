4755c82b6462280cf483ef2200b6ecd4f840e743
Angela Blanes <angelablanes@goldcar.tech>
2021-06-10 08:55:42 +0200

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    <p>Ruby</p>
+    <p class="textWeb">Ruby</p>
   </div>
 </template>
 
@@ -10,3 +10,15 @@ export default Vue.extend({
   name: 'Footer',
 });
 </script>
+
+<style lang="scss">
+.footer {
+  background-color: #222;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  color: white;
+  text-align: center;
+}
+</style>
